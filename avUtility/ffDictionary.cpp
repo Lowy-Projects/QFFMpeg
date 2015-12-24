6ed6ee037472d26a487e4108891756279d2448ca
Lowy-Projects <lowy72@gmail.com>
2015-12-24 11:20:05 +0100

    Modellé alakítás
    Az osztály modellé alakítása, a könyebb használat érdekében.
    
    
    Signed-off-by: Lowy <lowy72@gmail.com>

--- avUtility/ffDictionary.cpp
+++ avUtility/ffDictionary.cpp
@@ -1,40 +1,76 @@
 #include <QObject>
 #include <QDebug>
+#include<QLineEdit>
 #include "ffDictionary.h"
 
+extern "C" {
+    typedef struct AVDictionary{
+        int                 count;
+        AVDictionaryEntry * elems;
 
-ffDictionaryModel::ffDictionaryModel(QObject *parent) : QObject(parent)
-{
-    m_Error.clear();
-    m_Dic.setParent(this);
+    } AVDictionary;
 }
 
-ffDictionaryModel::ffDictionaryModel(void *FFMpeg_Dict, QObject *parent) : QObject(parent)
+ffDictionaryModel::ffDictionaryModel(QObject *parent) : QAbstractTableModel(parent)
 {
+    m_Error.clear();
+    m_Delegate = new ffDictionaryDelegate(this);
+    if (!m_Delegate)
+        m_Error.setError(Error_NotEnoughMemory);
+}
+
+ffDictionaryModel::ffDictionaryModel(void *FFMpeg_Dict, QObject *parent) : QAbstractTableModel(parent)
+{
+    m_Error.clear();
+    m_Delegate = new ffDictionaryDelegate(); //this);
+    if (!m_Delegate)
+    {
+        m_Error.setError(Error_NotEnoughMemory);
+        return;
+    }
+
     set_ffMpegValue(FFMpeg_Dict);
 }
 
 ffDictionaryModel::~ffDictionaryModel()
 {
+    if (m_Delegate)
+        delete m_Delegate;
+
+    m_Items.clear();
 }
 
-void ffDictionaryModel::Add(QString key, QString value)
+void ffDictionaryModel::append(QString key, QString value)
 {
-    InsertRow(key, value);
+    ffDictionaryItem * item;
+
+    m_Error.clear();
+    item = new ffDictionaryItem(key, value, this);
+    if (!item)
+    {
+        m_Error.setError(Error_NotEnoughMemory);
+        return;
+    }
+
+    beginInsertRows(QModelIndex(), m_Items.count(), m_Items.count());
+    m_Items.append(item);
+    endInsertRows();
+//    InsertRow(key, value);
 }
 
-QString ffDictionaryModel::Value(QString key, bool isSensitive)
+QString ffDictionaryModel::value(QString key, bool isSensitive)
 {
-    QPair<QString, QString> row;
+    ffDictionaryItem * item;
 
+    m_Error.clear();
     Qt::CaseSensitivity sense = isSensitive ? Qt::CaseSensitive : Qt::CaseInsensitive;
 
-    for (int i = 0; i < m_Dic.rowCount(); i++)
+    for (int i = 0; i < m_Items.count(); i++)
     {
-        row = valuePair(i);
+        item = m_Items[i];
 
-        if (row.first.compare(key, sense) == 0)
-            return row.second;
+        if (item->Key().compare(key, sense) == 0)
+            return item->Value();
     }
 
     return QString();
@@ -42,52 +78,104 @@ QString ffDictionaryModel::Value(QString key, bool isSensitive)
 
 void ffDictionaryModel::ffMpegValue(void **FFMpeg_Dict)
 {
-    QPair<QString, QString> item;
-    AVDictionary                        **dict = (AVDictionary**)FFMpeg_Dict;
+    ffDictionaryItem *item;
 
-    *dict = NULL;
-    for(int i = 0; i < m_Dic.rowCount(); i++)
+    m_Error.clear();
+
+    for(int i = 0; i < m_Items.count(); i++)
     {
-        item = valuePair(i);
-        if (av_dict_set(dict, item.first.toUtf8(), item.second.toUtf8(), 0) < 0)
-            qDebug() << "ffDictionary::Value(**ffMpeg_Dict) error: av_dict_set error.";
+        item = m_Items[i];
+        item->ffMpegValue(FFMpeg_Dict);
+
+        if (item->isError())
+        {
+            m_Error.copyFrom(item->Error());
+            break;
+        }
     }
 }
 
-QList<QPair<QString, QString> > ffDictionaryModel::List()
+QList<ffDictionaryItem *> ffDictionaryModel::List()
 {
-    QList<QPair<QString, QString> > list;
-
-    for(int i = 0; i < m_Dic.rowCount(); i++)
-        list.append(valuePair(i));
-
-    return list;
+    return m_Items;
 }
 
 void ffDictionaryModel::set_ffMpegValue(void *FFMpeg_Dict)
 {
+    ffDictionaryItem   *item;
     AVDictionary       *ffDic = (AVDictionary*)FFMpeg_Dict;
-    AVDictionaryEntry  *entrys = ffDic->elems;
-    int                 i, count = ffDic->count;
+    AVDictionaryEntry  *entrys = NULL;
+    int                 i = 0, count = 0;
 
+    m_Error.clear();
+
+    // Régi elemek törlése, ha van.
+    if (m_Items.size())
+    {
+        beginRemoveRows(QModelIndex(), 0, m_Items.size() - 1);
+        for(int i = 0; i < m_Items.size(); i++)
+        {
+            item = m_Items[0];
+            m_Items.removeAt(0);
+            delete item;
+        }
+        endRemoveRows();
+    }
+
+    // Új elemek felvétele
+    if (ffDic == NULL)
+        return;
+
+    entrys = ffDic->elems;
+    count  = ffDic->count;
+
+    beginInsertRows(QModelIndex(), 0, count-1);
     for(i = 0; i < count; i++)
-        InsertRow(entrys[i].key, entrys[i].value);
+    {
+        item = new ffDictionaryItem(entrys[i].key, entrys[i].value, this);
+        if ( !item || item->isError())
+            break;
+
+        m_Items.append(item);
+    }
+    endInsertRows();
+
+    if (i != count)
+    {
+        if (item)
+            m_Error.copyFrom(item->Error());
+        else
+            m_Error.setError(Error_NotEnoughMemory);
+    }
 }
 
-void ffDictionaryModel::setValue(QString key, QString value)
+void ffDictionaryModel::setValue(QString key, QString value, bool isSensitive)
 {
-    QStandardItem* item = valueItem(key);
+    ffDictionaryItem * item = NULL;
+    Qt::CaseSensitivity sens = (isSensitive == true) ? Qt::CaseSensitive : Qt::CaseInsensitive;
+
+    int i;
+
+    m_Error.clear();
+
+    for (i = 0; i < m_Items.size(); i++)
+        if (m_Items[i]->Key().compare(key ,sens) == 0)
+            item = m_Items[i];
 
     if (item)
     {
-        item->setData(value, 0);
+        item->setValue(value);
     }
     else
     {
-        Add(key, value);
+        item = new ffDictionaryItem(key, value, this);
+        beginInsertRows(QModelIndex(), m_Items.size(), m_Items.size());
+        m_Items.append(item);
+        endInsertRows();
     }
 }
 
+/*
 void ffDictionaryModel::setFromList(QList<QPair<QString, QString> > list)
 {
     m_Dic.clear();
@@ -101,13 +189,114 @@ void ffDictionaryModel::addFromList(QList<QPair<QString, QString> > list)
     for(i = 0; i < list.count(); i++)
         InsertRow(list[i].first, list[i].second);
 }
+*/
+
+// Modell kapcsolattartó függvények
+
+int ffDictionaryModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+
+    return m_Items.count();
+}
+
+int ffDictionaryModel::columnCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+
+    return 2;
+}
+
+QVariant ffDictionaryModel::data(const QModelIndex &index, int role) const
+{
+    QString str;
+    int row = index.row(), column = index.column();
+
+    switch (role) {
+        case Qt::DisplayRole:                                           // Cella megjelenítendő szövege
+        case Qt::EditRole:                                              // Cella szerkesztéshez megjelenítendő szöveg
+            if (column == 0)
+                return m_Items[row]->Key();
+            if (column == 1)
+                return m_Items[row]->Value();
+            break;
+
+    case Qt::TextAlignmentRole:
+        return (Qt::AlignLeft + Qt::AlignHCenter);
+
+    }
+    return QVariant();
+}
+
+bool ffDictionaryModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    if (index.column() == 0)
+        return true;
+
+    if (role == Qt::EditRole)
+    {
+        m_Items[index.row()]->setValue(value.toString());
+        emit dictionaryValueChanged(m_Items[index.row()]);
+
+        return true;
+    }
+    return false;
+}
+
+QVariant ffDictionaryModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (orientation == Qt::Horizontal)
+    {
+        switch(role)
+        {
+            case Qt::DisplayRole:
+
+                switch (section)
+                {
+                case 0:
+                    return m_KeyName;
+                case 1:
+                    return m_ValueName;
+                }
+        }
+    }
+    else
+    {
+        if (role == Qt::DisplayRole)
+            return section + 1;
+    }
+
+    return QVariant();
+}
+
+Qt::ItemFlags ffDictionaryModel::flags(const QModelIndex &index) const
+{
+    Qt::ItemFlags flag = QAbstractTableModel::flags(index);
+
+    if (index.column() == 1)
+        flag |= Qt::ItemIsEditable;
+
+    return flag;
+}
+
+bool ffDictionaryModel::removeRows(int row, int count, const QModelIndex &parent)
+{
+    beginRemoveRows(parent, row, row+count-1);
+
+    for(int i = 0; i < count; i++)
+        m_Items.removeAt(row);
+
+    endRemoveRows();
+    return true;
+}
 
 
 QString ffDictionaryModel::operator[](QString key)
 {
-    return Value(key);
+    return value(key);
 }
 
+/*
 bool ffDictionaryModel::InsertRow(QString s1, QString s2)
 {
     QList<QStandardItem *> row;
@@ -160,5 +349,56 @@ QStandardItem *ffDictionaryModel::valueItem(QString key, bool isSensitive)
     }
 
     return item;
+}
+
+*/
+
+
+//////////////////////////////////
+/// Delegát osztály megvalósítása
+//////////////////////////////////
+ffDictionaryDelegate::ffDictionaryDelegate(QObject *parent) :
+    QStyledItemDelegate(parent)
+{
+}
+
+QWidget *ffDictionaryDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    QLineEdit* editor = NULL;
+    QPalette   palette;
+
+    if (index.column() == 1)        // Érték módosítása
+    {
+        editor = new QLineEdit(parent);
+        palette = editor->palette();
+        palette.setColor(QPalette::Base,option.palette.color(QPalette::Highlight));
+        palette.setColor(QPalette::Text, option.palette.color(QPalette::HighlightedText));
+        editor->setPalette(palette);
+        editor->setAutoFillBackground(true);
+    }
+
+    return editor;
+}
+
+void ffDictionaryDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
+{
+    QString value = index.model()->data(index, Qt::EditRole).toString();
+    QLineEdit* lineEditor = static_cast<QLineEdit*>(editor);
+
+    lineEditor->setText(value);
+}
+
+void ffDictionaryDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const
+{
+    QLineEdit* lineEditor = static_cast<QLineEdit*>(editor);
+
+    model->setData(index, lineEditor->text(), Qt::EditRole);
+}
+
+void ffDictionaryDelegate::updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    Q_UNUSED(index)
+
+    editor->setGeometry(option.rect);
 }
 
--- avUtility/ffDictionary.h
+++ avUtility/ffDictionary.h
@@ -2,6 +2,12 @@
 #define FFDICTIONARY_H
 
 #include "qffmpeg_global.h"
+
+#include <QAbstractTableModel>
+#include <QStyledItemDelegate>
+#include <QList>
+#include "ffdictionaryitem.h"
+
 #include <QObject>
 #include <QStandardItem>
 #include <QStandardItemModel>
@@ -10,7 +16,9 @@
 #include <LowyLib/LowyLib.h>
 #include "ffdictionaryitem.h"
 
-class QFFMPEGSHARED_EXPORT ffDictionaryModel : public QObject
+class ffDictionaryDelegate;
+
+class QFFMPEGSHARED_EXPORT ffDictionaryModel : public QAbstractTableModel
 {
     Q_OBJECT
 public:
@@ -18,17 +26,28 @@ public:
     explicit ffDictionaryModel(void * FFMpeg_Dict, QObject *parent = 0);
     ~ffDictionaryModel();
 
-    void     Add(QString key, QString value);
-    void     Clear();
-    int      Count() {return m_Dic.rowCount();}
-    QString  Value(QString key, bool isSensitive = true);
+    void     append(QString key, QString value);
+    void     clear();
+    int      count() {return m_Items.count();}
+    QString  value(QString key, bool isSensitive = true);
 
-    QStandardItemModel*     Model() {return &m_Dic;}
+//    QStandardItemModel*     Model() {return &m_Dic;}
 
-    QList<QPair<QString, QString> > List();
-    void                    setValue(QString key, QString value);
-    void                    setFromList(QList<QPair<QString, QString> >list);
-    void                    addFromList(QList<QPair<QString, QString> > list);
+    ffDictionaryDelegate *   delegate() {return m_Delegate;}
+    QList<ffDictionaryItem*> List();
+    void                    setValue(QString key, QString value, bool isSensitive = true);
+//    void                    setFromList(QList<QPair<QString, QString> >list);
+//    void                    addFromList(QList<QPair<QString, QString> > list);
+
+    // Model tagfügvények
+    int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE ;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+    bool     setData(const QModelIndex &index, const QVariant &value, int role) Q_DECL_OVERRIDE;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const Q_DECL_OVERRIDE;
+    Qt::ItemFlags flags(const QModelIndex &index) const Q_DECL_OVERRIDE;
+    bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) Q_DECL_OVERRIDE;
+
 
 /***********************************
  * ffMpeg kapcsolattartó fügvények.
@@ -43,16 +62,43 @@ public:
     QString operator[] (QString key);
 
 signals:
+    void dictionaryValueChanged(ffDictionaryItem *item);
 
 public slots:
 
 private:
-    bool                        InsertRow(QString s1, QString s2);
-    QPair<QString, QString>     valuePair(int num);
-    QStandardItem               *valueItem(QString key,  bool isSensitive = true);
+//    bool                        InsertRow(QString s1, QString s2);
+//    QPair<QString, QString>     valuePair(int num);
+//    QStandardItem               *valueItem(QString key,  bool isSensitive = true);
 
-    QStandardItemModel          m_Dic;
+    QString                     m_KeyName;
+    QString                     m_ValueName;
+    QList<ffDictionaryItem *>   m_Items;
+    ffDictionaryDelegate*       m_Delegate;
     lError                      m_Error;
+};
+
+//
+/// Delegát osztály a szerkesztéshez
+//
+
+class ffDictionaryDelegate: public QStyledItemDelegate
+{
+    Q_OBJECT
+
+public:
+    ffDictionaryDelegate(QObject * parent = 0);
+
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
+                              const QModelIndex &index) const Q_DECL_OVERRIDE;
+
+    void setEditorData(QWidget *editor, const QModelIndex &index) const Q_DECL_OVERRIDE;
+    void setModelData(QWidget *editor, QAbstractItemModel *model,
+                          const QModelIndex &index) const Q_DECL_OVERRIDE;
+
+    void updateEditorGeometry(QWidget *editor,
+                          const QStyleOptionViewItem &option, const QModelIndex &index) const Q_DECL_OVERRIDE;
+
 };
 
 #endif // FFDICTIONARY_H
